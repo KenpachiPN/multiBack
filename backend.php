a7604fdc4264b100d61fdc51b71036c2a0d0a0f8
SantiagoPN <sprada71@misena.edu.co>
2024-01-26 02:58:26 -0500

    fix: archivos

--- backend.php
+++ backend.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+
 header('Content-Type: application/json');
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
@@ -8,6 +11,8 @@ header('Access-Control-Allow-Headers: Content-Type');
 
 // Directorio donde se guardará el archivo en el servidor
 $directorioDestino = __DIR__ . '/Multimedia/';
+$rutaRelativa = '/Multimedia/';
+
 
 // Recibe los datos del formulario
 $user = isset($_POST['usuario']) ? trim(strtolower($_POST['usuario'])) : '';
@@ -16,49 +21,42 @@ $deposito = isset($_POST['deposito']) ? $_POST['deposito'] : '';
 $recaptchaResponse = isset($_POST['recaptchaResponse']) ? $_POST['recaptchaResponse'] : '';
 $multi = isset($_FILES['files']) ? $_FILES['files'] : null;
 
-
 if (empty($user) || empty($passwd) || empty($deposito) || empty($recaptchaResponse) || $multi === null) {
     echo json_encode(['success' => false, 'message' => '¡Hay campos vacios!']);
-    exit;
-}
-// Mueve el archivo a la ubicación
-$nombreArchivo = $multi['name'];
-$rutaDestino = $directorioDestino . $nombreArchivo;
-$rutaTemporal = $multi['tmp_name'];
-$estadoMulti = false;
-
-if (move_uploaded_file($rutaTemporal, $rutaDestino)) {
-    $estadoMulti = true;
-} else {
-    $estadoMulti = false;
 }
 
-echo json_encode(['estad' => $estadoMulti]);
+$estadoMulti;
 
-if ($estadoMulti) {
-    $formData = [
-        'success' => true,
-        'message' => 'Multimedia guardada éxitosamente',
-        'data' => [
-            'user' => $user,
-            'pass' => $passwd,
-            'deposito' => $deposito,
-            'responCap' => $recaptchaResponse,
-            'Multimedia' => [
-                'status' => $estadoMulti,
-                'ruta' => $rutaDestino
+$files = [];
+foreach ($_FILES['files']['name'] as $i => $files_name) {
+    $rutaDestino = __DIR__ . $rutaRelativa . basename($files_name);
+    if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $rutaDestino)) {
+        $files[] = [
+            'name' => $files_name,
+            'ruta' => $rutaRelativa . $files_name
+        ];
+        $formData = [
+            'success' => true,
+            'message' => 'Multimedia guardada éxitosamente',
+            'data' => [
+                'user' => $user,
+                'pass' => $passwd,
+                'deposito' => $deposito,
+                'responCap' => $recaptchaResponse,
+                'Multimedia' => $files
             ],
-        ],
 
-    ];
-} else {
-    $formData = [
-        'success' => false,
-        'message' => 'La multimedia no se guardó'
-    ];
+        ];
+        echo json_encode($formData);
+    } else {
+        $formData = [
+            'success' => false,
+            'message' => 'La multimedia no se guardó'
+        ];
+        echo json_encode($formData);
+    }
 }
 
-echo json_encode($formData);
 exit;
 
 ?>
