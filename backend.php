abe61408fd83f93c2e39581eb3d9a35c6bae35e3
SantiagoPN <sprada71@misena.edu.co>
2024-01-26 02:00:39 -0500

    fix: new camb

--- backend.php
+++ backend.php
@@ -7,56 +7,58 @@ header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
 // Directorio donde se guardará el archivo en el servidor
-$directorioDestino = '/MultimediaBack/Multimedia/';
+$directorioDestino = __DIR__ . '/Multimedia/';
 
 // Recibe los datos del formulario
 $user = isset($_POST['usuario']) ? trim(strtolower($_POST['usuario'])) : '';
-$passwd = isset($_POST['contrasena']) ? trim(strtolower($_POST['contrasena'])) : '';
+$passwd = isset($_POST['contrasena']) ? trim($_POST['contrasena']) : '';
 $deposito = isset($_POST['deposito']) ? $_POST['deposito'] : '';
 $recaptchaResponse = isset($_POST['recaptchaResponse']) ? $_POST['recaptchaResponse'] : '';
-$multi = isset($_FILES['multimedia']) ? $_FILES['multimedia'] : null;
+$multi = isset($_FILES['files']) ? $_FILES['files'] : null;
 
 
-$formData = [
-    'succes' => true,
-    'message' => 'back',
-    'data'=> [
-        'user'=> $user,
-        'pass' => $passwd,
-        'deposito' => $deposito,
-        'responCap' => $recaptchaResponse,
-        'Multimedia' => $multi
-    ],
-];
+if (empty($user) || empty($passwd) || empty($deposito) || empty($recaptchaResponse) || $multi === null) {
+    echo json_encode(['success' => false, 'message' => '¡Hay campos vacios!']);
+    exit;
+}
+// Mueve el archivo a la ubicación
+$nombreArchivo = $multi['name'];
+$rutaDestino = $directorioDestino . $nombreArchivo;
+$rutaTemporal = $multi['tmp_name'];
+$estadoMulti = false;
+
+if (move_uploaded_file($rutaTemporal, $rutaDestino)) {
+    $estadoMulti = true;
+} else {
+    $estadoMulti = false;
+}
+
+echo json_encode(['estad' => $estadoMulti]);
+
+if ($estadoMulti) {
+    $formData = [
+        'success' => true,
+        'message' => 'Multimedia guardada éxitosamente',
+        'data' => [
+            'user' => $user,
+            'pass' => $passwd,
+            'deposito' => $deposito,
+            'responCap' => $recaptchaResponse,
+            'Multimedia' => [
+                'status' => $estadoMulti,
+                'ruta' => $rutaDestino
+            ],
+        ],
+
+    ];
+} else {
+    $formData = [
+        'success' => false,
+        'message' => 'La multimedia no se guardó'
+    ];
+}
 
 echo json_encode($formData);
-
-
-
-
-
-
-
-
-
-
-
-// // Verifica si se ha subido un archivo
-// if ($multi !== null && $multi['error'] == UPLOAD_ERR_OK) {
-//     // Obtiene la información del archivo
-//     $nombreArchivo = basename($multi['name']);
-//     $rutaTemporal = $multi['tmp_name'];
-
-//     // Mueve el archivo a la ubicación
-//     $rutaDestino = $directorioDestino . $nombreArchivo;
-//     if (move_uploaded_file($rutaTemporal, $rutaDestino)) {
-//         echo json_encode(['success' => true, 'message' => 'Multimedia enviada con éxito']);
-//     } else {
-//         echo json_encode(['success' => false, 'message' => 'Error al mover el archivo']);
-//     }
-// } else {
-//     echo json_encode(['success' => false, 'message' => 'tranza']);
-// }
-
 exit;
+
 ?>
