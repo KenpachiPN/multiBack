ff8f0a631b00f384b0abba990051b3c4687ecb3e
SantiagoPN <sprada71@misena.edu.co>
2024-01-29 13:08:47 -0500

    fix: cambio ruta deposito

--- backend.php
+++ backend.php
@@ -1,13 +1,9 @@
 <?php
-phpinfo();
 header('Content-Type: application/json');
 header('Access-Control-Allow-Headers: Content-Type');
 
-
 // Directorio donde se guardará el archivo en el servidor
 $directorioDestino = __DIR__ . '/Multimedia/';
-$rutaRelativa = '/Multimedia/';
-
 
 // Recibe los datos del formulario
 $user = isset($_POST['usuario']) ? trim(strtolower($_POST['usuario'])) : '';
@@ -16,13 +12,28 @@ $deposito = isset($_POST['deposito']) ? $_POST['deposito'] : '';
 $recaptchaResponse = isset($_POST['recaptchaResponse']) ? $_POST['recaptchaResponse'] : '';
 $multi = isset($_FILES['files']) ? $_FILES['files'] : null;
 
+// Directorio donde se guardará el archivo en el servidor
+$rutaRelativa = '/Multimedia/' . $deposito . '/';
+
 if (empty($user) || empty($passwd) || empty($deposito) || empty($recaptchaResponse) || $multi === null) {
-    echo json_encode(['success' => false, 'message' => '¡Hay campos vacios!']);
+    $formDataErr = [
+        'success' => false,
+        'message' => 'Error interno',
+        'data' => [
+            'user' => $user,
+            'pass' => $passwd,
+            'deposito' => $deposito,
+            'responCap' => $recaptchaResponse,
+            'Multimedia' => $multi
+        ],
+    ];
+    echo json_encode($formDataErr);
 }
 
 $estadoMulti;
 
 $files = [];
+
 foreach ($_FILES['files']['name'] as $i => $files_name) {
     $rutaDestino = __DIR__ . $rutaRelativa . basename($files_name);
     if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $rutaDestino)) {
