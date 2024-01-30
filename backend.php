479b1fa07b531d6fa5532660e5edb7d08e5f6216
SantiagoPN <sprada71@misena.edu.co>
2024-01-30 15:15:30 -0500

    Fix: Guardar en json

--- backend.php
+++ backend.php
@@ -11,9 +11,14 @@ $passwd = isset($_POST['contrasena']) ? trim($_POST['contrasena']) : '';
 $deposito = isset($_POST['deposito']) ? $_POST['deposito'] : '';
 $recaptchaResponse = isset($_POST['recaptchaResponse']) ? $_POST['recaptchaResponse'] : '';
 $multi = isset($_FILES['files']) ? $_FILES['files'] : null;
+//Seteamos a la hora de nuestro pais
+date_default_timezone_set('America/Bogota');
+$fecha = date("Y-m-d H:i:s");
 
 // Directorio donde se guardará el archivo en el servidor
 $rutaRelativa = '/Multimedia/' . $deposito . '/';
+// Directorio donde está el JSON
+$rutaJson = '/Multimedia/' . $deposito . '/' . $deposito . '.json';
 
 if (empty($user) || empty($passwd) || empty($deposito) || empty($recaptchaResponse) || $multi === null) {
     $formDataErr = [
@@ -28,14 +33,19 @@ if (empty($user) || empty($passwd) || empty($deposito) || empty($recaptchaRespon
         ],
     ];
     echo json_encode($formDataErr);
+    exit;
 }
-
-$estadoMulti;
 
 $files = [];
 
+// Obtener los datos del JSON
+$fileJson = json_decode(file_get_contents($rutaJson));
+
+
+
 foreach ($_FILES['files']['name'] as $i => $files_name) {
     $rutaDestino = __DIR__ . $rutaRelativa . basename($files_name);
+    $rutaDestinoJson = __DIR__ . $rutaJson;
     if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $rutaDestino)) {
         $files[] = [
             'name' => $files_name,
@@ -49,10 +59,17 @@ foreach ($_FILES['files']['name'] as $i => $files_name) {
                 'pass' => $passwd,
                 'deposito' => $deposito,
                 'responCap' => $recaptchaResponse,
-                'Multimedia' => $files
+                'Multimedia' => $files,
+                'Fecha subida' => $fecha
             ],
-
         ];
+        // Agregar los nuevos datos al array
+        $fileJson[] = $formData['data'];
+        // Codificar el array completo en formato JSON
+        $jsonData = json_encode($fileJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        // Guardar el array completo en el archivo JSON
+        file_put_contents($rutaDestinoJson, $jsonData);
+        //Mostrar la respuesta
         echo json_encode($formData);
     } else {
         $formData = [
